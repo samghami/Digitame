8f4d796ec294ed341227b1dac1a3315d18f2c4d3
samghami <samghami@gmail.com>
2015-07-02 03:19:12 +0200

    Création de l'index du site

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+/* ##########################################
+##    (c) 2015 Depaule Julien              ##
+##        Alias samghami                   ##
+##                                         ##
+##        contact@samghami.fr.nf           ##
+##        http://www.samghami.fr.nf/       ##
+##                                         ##
+##        CMS de système de comptabilité   ##
+##        Delamare Deboutteville           ##
+##        Il est sous licence GNU          ##
+########################################## */
+
+/* ##########################################
+## Pages index.php
+########################################## */
+
+require('kernel/includes.php');
