33c7acaa5694ce65ab2b349065ea62ddd65ecb67
samghami <samghami@gmail.com>
2015-07-02 02:56:52 +0200

    création includes

---
+++ kernel/includes.php
@@ -0,0 +1,20 @@
+<?php 
+/* ##########################################
+##    (c) 2015 Depaule Julien              ##
+##        Alias samghami                   ##
+##                                         ##
+##        contact@samghami.fr.nf           ##
+##        http://www.samghami.fr.nf/       ##
+##                                         ##
+##        CMS de système de comptabilité   ##
+##        Delamare Deboutteville           ##
+##        Il est sous licence GNU          ##
+########################################## */
+
+/* ##########################################
+## Pages includes.php
+########################################## */
+
+require_once('conf/BDD/bdd.php');
+require_once('conf/conf.php');
+require_once('functions/SelectAllGeneralFunctions.php');
